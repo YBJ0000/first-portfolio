99896503f409c3a16b738f4403a8c3bba1c21e44
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2025-04-12 03:57:46 +1000

    optimize seo

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
   title: 'Bingjia Yang | Developer/Designer/Dreamer/Doer',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    'I am a highly adaptable Full Stack Developer with experience building scalable web apps using modern frameworks and cloud tools like React, Next.js, Node.js, Docker, and AWS EC2. I enjoy working across both frontend and backend, and I thrive in cross-functional teams where I can solve real-world problems with clean, scalable code.',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://first-portfolio-topaz-chi.vercel.app/'
       : 'http://localhost:3000'
   ),
 } as const
